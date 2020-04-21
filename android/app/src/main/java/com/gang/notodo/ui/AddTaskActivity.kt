d6c0b771d01f89e5b8e47ba544fd11d90d1144d6
lixinyang <741162208@qq.com>
2020-04-21 19:49:20 +0800

    修小bug

--- android/app/src/main/java/com/gang/notodo/ui/AddTaskActivity.kt
+++ android/app/src/main/java/com/gang/notodo/ui/AddTaskActivity.kt
@@ -5,12 +5,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import androidx.appcompat.widget.Toolbar
 import com.gang.notodo.R
+import com.gang.notodo.ui.calendar.CalendarActivity
+import com.gang.notodo.util.setupActionBar
+import com.gang.notodo.util.startActivity
 
 class AddTaskActivity : AppCompatActivity() {
 
     private lateinit var title: EditText
     private lateinit var description: EditText
+    private lateinit var mToolBar: Toolbar
     private var year: Int = 0
     private var month: Int = 0
     private var day: Int = 0
@@ -29,8 +35,31 @@ class AddTaskActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-//        title = findViewById(R.id.add_task_title)
-//        description = findViewById(R.id.add_task_description)
+        title = findViewById(R.id.add_task_title)
+        description = findViewById(R.id.add_task_description)
+        mToolBar = findViewById(R.id.toolBar)
+        initToolBar()
+    }
+
+    private fun initToolBar() {
+        setupActionBar(R.id.toolBar) {
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+            setDisplayHomeAsUpEnabled(true)
+            title = "NO-todo"
+        }
+        mToolBar.setNavigationOnClickListener { v ->
+            val popup = PopupMenu(this, v)
+            val inflater = popup.menuInflater
+            inflater.inflate(R.menu.menu_indicator, popup.menu)
+            popup.show()
+            popup.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.item_calendar -> startActivity<CalendarActivity>()
+                    R.id.item_todo -> startActivity<ListActivity>()
+                }
+                false
+            }
+        }
     }
 
     companion object {
