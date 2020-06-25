87160da77c3d79101b32b707d3dae3398b7e3986
lixinyang <741162208@qq.com>
2020-06-25 21:16:47 +0800

    数据库添加表

--- android/app/src/main/java/com/gang/notodo/data/local/AppDatabase.kt
+++ android/app/src/main/java/com/gang/notodo/data/local/AppDatabase.kt
@@ -5,26 +5,29 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.gang.notodo.data.Task
+import com.gang.notodo.data.User
 
 
-@Database(entities = [Task::class], version = 1)
-abstract class TaskDatabase : RoomDatabase() {
+@Database(entities = [Task::class, User::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
 
     abstract fun taskDao(): TaskDao
 
+    abstract fun userDao(): UserDao
+
     companion object {
 
-        private var instance: TaskDatabase? = null
+        private var instance: AppDatabase? = null
 
         private val LOCK = Any()
 
-        fun getInstance(context: Context): TaskDatabase {
+        fun getInstance(context: Context): AppDatabase {
             if (instance == null) {
                 synchronized(LOCK) {
                     if (instance == null) {
                         instance = Room.databaseBuilder(
                             context.applicationContext,
-                            TaskDatabase::class.java,
+                            AppDatabase::class.java,
                             "tasks.db"
                         )
                             .build()
